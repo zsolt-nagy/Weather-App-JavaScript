c71f15de5d4be72f6f42e35d7ed487b3e0570095
Zsolt Nagy <info@zsoltnagy.eu>
2023-07-01 01:17:27 +0200

    weather codes

--- weather.js
+++ weather.js
@@ -4,6 +4,37 @@ let errorField = document.querySelector(".js-error");
 let citySection = document.querySelector(".js-city-data");
 let weatherSection = document.querySelector(".js-weather-data");
 
+let weatherCodeMap = {
+    0: "Clear sky",
+    1: "Mainly clear",
+    2: "Partly cloudy",
+    3: "Overcast",
+    45: "Fog",
+    48: "Depositing rime fog",
+    51: "Drizzle: Light intensity",
+    53: "Drizzle: Moderate intensity",
+    55: "Drizzle: Dense intensity",
+    56: "Freezing Drizzle: Light intensity",
+    57: "Freezing Drizzle: Dense intensity",
+    61: "Rain: Slight intensity",
+    63: "Rain: Moderate  intensity",
+    65: "Rain: Heavy intensity",
+    66: "Freezing Rain: Light  intensity",
+    67: "Freezing Rain: Geavy intensity",
+    71: "Snow fall: Slight intensity",
+    73: "Snow fall: Moderate intensity",
+    75: "Snow fall: Heavy intensity",
+    77: "Snow grains",
+    80: "Rain showers: Slight",
+    81: "Rain showers: Moderate",
+    82: "Rain showers: violent",
+    85: "Snow showers slight",
+    86: "Snow showers heavy",
+    95: "Thunderstorm: Slight",
+    96: "Thunderstorm: Moderate",
+    99: "Thunderstorm with slight and heavy hail",
+};
+
 function getCityEndpoint(cityName) {
     return `https://geocoding-api.open-meteo.com/v1/search?name=${cityName}`;
 }
@@ -37,6 +68,7 @@ function generate7DayForecast(daily, dailyUnits) {
                     <th class="weather-table-cell">Sunrise</th>
                     <th class="weather-table-cell">Sunset</th>
                     <th class="weather-table-cell">Precipitation</th>
+                    <th class="weather-table-cell">Weather Code</th>
                 </tr>
             </thead>
             <tbody>    
@@ -51,6 +83,7 @@ function generate7DayForecast(daily, dailyUnits) {
                 <td class="weather-table-cell">${getTimeFromIso8601(daily.sunrise[i])}</td>
                 <td class="weather-table-cell">${getTimeFromIso8601(daily.sunset[i])}</td>
                 <td class="weather-table-cell">${daily.precipitation_sum[i]} ${dailyUnits.precipitation_sum}</td>
+                <td class="weather-table-cell">${weatherCodeMap[daily.weathercode[i]] ?? "-"}</td>
             </tr>
         `;
     }
